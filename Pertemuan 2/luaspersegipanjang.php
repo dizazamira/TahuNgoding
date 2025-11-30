55f78c2c5a9bcbd7243768e7fdd70f9649e4b7c4
dizazamira <dizahazmi2016@gmail.com>
2025-11-30 18:53:14 +0700

    Upload folder Pertemuan 1 dan Pertemuan 2 beserta file

---
+++ Pertemuan 2/luaspersegipanjang.php
@@ -0,0 +1,10 @@
+<?php
+
+$panjang = 8;
+$lebar = 5;
+
+function luas($panjang, $lebar) {
+    return $panjang * $lebar;
+}
+echo "Luas persegi panjang adalah " . luas($panjang, $lebar) . " dengan panjang " . $panjang . " dan lebar " . $lebar . ".";
+?>
